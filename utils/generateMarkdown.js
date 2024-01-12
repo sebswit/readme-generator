4de28ec68ae7597da15ab6dc180bf8d6d214a0e8
sebswit <sebastian.switakowski@gmail.com>
2024-01-12 19:51:13 +0000

    generateMarkdown structure first lines

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,6 +2,19 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ## Description
+
+  ${data.description}
+
+  ## Table of Contents
+
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
 `;
 }
 
