19d9956577b5da17bd73f134bdbbcc5a04767584
sebswit <sebastian.switakowski@gmail.com>
2024-01-12 19:25:29 +0000

    added questions, writeToFile, init and getLicenceBadge

--- index.js
+++ index.js
@@ -5,17 +5,74 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide description for your project'  
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the installation instructions?'  
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide usage information'  
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license for your project:',
+        choices: ['MIT', 'Apache-2.0', 'GPL-3.0', 'BSD-3-Clause', 'None'], 
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Provide contributors guideline'  
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Provide test instructions'  
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Provide GitHub username'  
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?'  
+    },
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+    const outputPath = path.join(process.cwd(), fileName);
+
+    fs.writeFile(outputPath, data, (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+    });
 }
 
 // function to initialize program
 function init() {
-
-}
+    try{
+ const answers = inquirer.prompt(questions);
+ const markdown = generateMarkdown(answers);
+    writeToFile('README.md', markdown);
+    } catch(err) {
+        console.log(err);
+    }
+};
 
 // function call to initialize program
 init();
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,22 @@
+// function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+
+`;
+}
+
+function getLicenseBadge(license) {
+  if (license === 'MIT') {
+    return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+  } else if (license === 'Apache-2.0') {
+    return '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+  } else if (license === 'GPL-3.0') {
+    return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+  } else if (license === 'BSD-3-Clause') {
+    return '[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)';
+  } else {
+    return 'This product is not licensed. ';
+  }
+};
+module.exports = generateMarkdown;
+//licences
